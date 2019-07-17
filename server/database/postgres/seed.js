ebacae237140f26c756e7176a52a2a3ce73004da
David Nguyen <tnguyen.david@gmail.com>
2019-07-16 17:54:39 -0700

    Import faker to seed postgres

--- server/database/postgres/seed.js
+++ server/database/postgres/seed.js
@@ -1,4 +1,12 @@
 const db = require('./index.js');
 const faker = require('faker');
 
-console.log(db);
+const seed = () => {
+  for (let i = 0; i < 1000000; i++) {
+    db.query(`INSERT INTO questions (id, question, product_id) VALUES(${question})`);
+    const question = faker.lorem.sentence();
+    const response = faker.lorem.paragraph();
+  }
+}
+
+seed();
