c39c60067cc85be0e68457093e5afd780a1a2dc4
David Nguyen <tnguyen.david@gmail.com>
2019-07-30 15:15:20 -0700

    Changed seeding to csv format

--- server/database/seeding/fakerSeed.js
+++ server/database/seeding/fakerSeed.js
@@ -1,26 +1,21 @@
 const faker = require('faker');
 const fs = require('fs');
+const json2csv = require('json2csv').parse;
 
 const questions = (count, iter, cb) => {
-  const questions = [];
+  let questions = '';
 
   for (let i = 1; i <= count; i++) {
-    questions.push({
-      id: i + (count * iter),
-      question: `${faker.lorem.sentence().slice(0, -1)}?`,
-      response: faker.lorem.paragraph(),
-      votes: Math.floor(Math.random() * 1000),
-      product_id: Math.floor(Math.random() * 20000)
-    });
+    questions += `${i + (count * iter)}, ${faker.lorem.sentence().slice(0, -1)}?, ${faker.lorem.paragraph()}, ${Math.floor(Math.random() * 1000)}, ${Math.floor(Math.random() * 20000)}\n`;
   }
 
   cb(questions);
 };
 
 const seed = (() => {
-  for(let i = 0; i < 2; i++) {
-    questions(100000, i, (data) => {
-      fs.appendFileSync(`${__dirname}/fakerData.js`, JSON.stringify(data, null, '\t'))
+  for(let i = 0; i < 50000; i++) {
+    questions(2, i, (data) => {
+      fs.appendFileSync(`${__dirname}/fakerData.csv`, data)
     })
   }
 })();
