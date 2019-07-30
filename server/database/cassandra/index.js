c4e97d9dc817ab97269e9bf58b0f010fedf351a5
David Nguyen <tnguyen.david@gmail.com>
2019-07-30 13:23:54 -0700

    Complete creating keyspace/table for cassandra

--- server/database/cassandra/index.js
+++ server/database/cassandra/index.js
@@ -6,14 +6,17 @@ const client = new cassandra.Client({
 
 client.connect()
   .then(() => {
-    const query = "CREATE KEYSPACE IF NOT EXISTS amazon WITH replication =" +
-      "{'class': 'SimpleStrategy', 'replication_factor': '1' }";
+    const query = `CREATE KEYSPACE IF NOT EXISTS amazon WITH replication =
+      {'class': 'SimpleStrategy', 'replication_factor': '1' }`;
     return client.execute(query);
   })
   .then(() => {
-    const query = "CREATE TABLE IF NOT EXISTS amazon.qna" +
-      " (id1 uuid, id2 timeuuid, txt text, val int, PRIMARY KEY(id1, id2))";
+    const query = `CREATE TABLE IF NOT EXISTS amazon.qna
+      (id1 uuid, question text, response text, votes int, product_id int, PRIMARY KEY(id1))`;
     return client.execute(query);
+  })
+  .then(() => {
+    console.log('Complete creating keyspace/table.')
   })
   .catch((err) => {
     console.error('There was an error', err);
