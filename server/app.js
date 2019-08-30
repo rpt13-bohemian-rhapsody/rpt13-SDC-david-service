7b9619c73e50e92bd588e9ee93b31ea6a9972c76
David Nguyen <tnguyen.david@gmail.com>
2019-08-30 00:02:42 -0700

    bundle

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const morgan = require('morgan');
 const app = express();
 // const db = require('../server/database/schema.js');
-// const db2 = require('./database/postgres/controllers/index.js');
+const db2 = require('./database/postgres/controllers/index.js');
 const cors = require('cors');
 const PORT = 3000;
 // require('newrelic');
@@ -19,18 +19,18 @@ app.use(cors());
 app.use(express.static(path.join(__dirname + '/../client/dist')));
 
 //allows to serve react files whenever the url Route is changed in the client side.
-// app.use('/products/:id', express.static(path.join(__dirname + '/../client/dist')));
+app.use('/products/:id', express.static(path.join(__dirname + '/../client/dist')));
 
-// // creates product
-// app.post('/products/questions/', db2.queries.post);
+// creates product
+app.post('/products/questions/', db2.queries.post);
 
-// // read/get all questions for specific product
-// app.get('/products/questions/:productId/', db2.queries.get);
+// read/get all questions for specific product
+app.get('/products/questions/:productId/', db2.queries.get);
 
-// // update votes on a question
-// app.put('/products/questions/votes/:question_id', db2.queries.put);
+// update votes on a question
+app.put('/products/questions/votes/:question_id', db2.queries.put);
 
-// // deletes product
-// app.delete('/products/questions/:productId', db2.queries.delete);
+// deletes product
+app.delete('/products/questions/:productId', db2.queries.delete);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
